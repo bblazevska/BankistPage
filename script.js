750d5d1ae9920721d78668c69f8b7031511d0358
kuzeva <kuzeva01@gmail.com>
2021-09-09 17:18:09 +0200

    revealing sections on scroll

--- script.js
+++ script.js
@@ -188,6 +188,7 @@ const stickyNav = function (entries) {
 
   if (!entry.isIntersecting) nav.classList.add('sticky');
   else nav.classList.remove('sticky');
+
 }
 
 const headerObserver = new IntersectionObserver(stickyNav, {
@@ -197,7 +198,26 @@ const headerObserver = new IntersectionObserver(stickyNav, {
 });
 headerObserver.observe(header);
 
+//  reveal sections
+const allSections = document.querySelectorAll('.section');
 
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+  console.log(entry);
+  if (!entry.isIntersecting) return;
+
+  entry.target.classList.remove('section--hidden');
+  observer.unobserve(entry.target);
+}
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+
+allSections.forEach(section => {
+  sectionObserver.observe(section);
+  section.classList.add('section--hidden')
+})
 
 
 /////////////////////////////////////////////////////////////
