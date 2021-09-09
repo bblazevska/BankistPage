7740fb4b15c60a2109558bfce119951b7fcd56c2
kuzeva <kuzeva01@gmail.com>
2021-09-09 15:36:44 +0200

    building tabbed component

--- script.js
+++ script.js
@@ -95,6 +95,35 @@ btnScrollTo.addEventListener('click', e => {
   // });
   */
 
+
+//Tabbed component
+
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+
+tabsContainer.addEventListener('click', function (e) {
+  e.preventDefault();
+  const clicked = e.target.closest('.operations__tab');
+
+  // Guard clause
+  if (!clicked) return;
+
+  // Remove active classes
+  tabs.forEach(t => t.classList.remove('operations__tab--active'));
+  tabsContent.forEach(c => c.classList.remove('operations__content--active'));
+  
+  // Active tab
+  clicked.classList.add('operations__tab--active');
+  
+  // Acrivate content area
+  document.querySelector(`.operations__content--${clicked.dataset.tab}`).classList.add('operations__content--active');
+  
+});
+
+
+
 /////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////
 // ------------------ LECTURES ----------------------
@@ -231,7 +260,7 @@ document.querySelector('.nav').addEventListener('click', function (e) {
 */
 
 // DOM Traversing
-
+/*
 const h1 = document.querySelector('h1');
 
 //Selecting elements 
@@ -265,3 +294,4 @@ console.log(h1.parentElement.children);
 [...h1.parentElement.children].forEach(function (el) {
   if (el !== h1) el.style.transform = 'scale(0.5)';
 });
+*/
